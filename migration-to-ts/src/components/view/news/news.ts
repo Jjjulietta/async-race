e582ceee1bf18c36461021edbdef3718f988ecde
Julia Ivashchanka <jjjuliettai@gmail.com>
2023-06-10 15:13:55 +0300

    fix: correct mistakes

--- migration-to-ts/src/components/view/news/news.ts
+++ migration-to-ts/src/components/view/news/news.ts
@@ -4,6 +4,7 @@ import { DataNews } from '../../../types/index';
 
 class News {
     public static draw(data: DataNews[]): void {
+        console.log(data);
         const news = data.length >= 10 ? data.filter((_item, idx: number) => idx < 10) : data;
 
         const fragment = document.createDocumentFragment();
@@ -11,25 +12,25 @@ class News {
         if (newsItemTemp instanceof HTMLTemplateElement) {
             news.forEach((item, idx: number) => {
                 const newsClone = newsItemTemp.content.cloneNode(true);
-                if (idx % 2 && newsClone instanceof HTMLElement) {
+                if (idx % 2 && newsClone instanceof DocumentFragment) {
                     const newsItem: ParamType = newsClone?.querySelector('.news__item');
                     if (newsItem) newsItem.classList.add('alt');
                 }
-                if (newsClone instanceof HTMLElement) {
+                if (newsClone instanceof DocumentFragment) {
                     const newsMetaPhoto: ParamType = newsClone?.querySelector('.news__meta-photo');
                     if (newsMetaPhoto)
                         newsMetaPhoto.style.backgroundImage = `url(${item.urlToImage || 'img/news_placeholder.jpg'})`;
                 }
-                if (newsClone instanceof HTMLElement) {
+                if (newsClone instanceof DocumentFragment) {
                     const newsMetaAuthor: ParamLi = newsClone?.querySelector('.news__meta-author');
                     if (newsMetaAuthor) newsMetaAuthor.textContent = item.author || item.source.name;
                 }
-                if (newsClone instanceof HTMLElement) {
+                if (newsClone instanceof DocumentFragment) {
                     const newsMetaDate: ParamLi = newsClone?.querySelector('.news__meta-date');
                     if (newsMetaDate)
                         newsMetaDate.textContent = item.publishedAt.slice(0, 10).split('-').reverse().join('-');
                 }
-                if (newsClone instanceof HTMLElement) {
+                if (newsClone instanceof DocumentFragment) {
                     const newsTitle = newsClone?.querySelector('.news__description-title');
                     if (newsTitle) newsTitle.textContent = item.title;
                     const newsSourse = newsClone?.querySelector('.news__description-source');
