4712d770e53eb2eeef0ab2781d0888515ddb93d1
Julia Ivashchanka <jjjuliettai@gmail.com>
2023-06-11 14:33:24 +0300

    fix: change link to api

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://rss-news-api.onrender.com/', {
             apiKey: '69e3536a6d1b43f28e620c088e33ff3e', // получите свой ключ https://newsapi.org/
         });
     }
