96f462f1d85b4b58de885f16715d37823b9c4cce
Julia Ivashchanka <jjjuliettai@gmail.com>
2023-07-03 22:08:05 +0300

    fix: change webpack config

--- rss-css-selectors/webpack.config.js
+++ rss-css-selectors/webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
-      { test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+      { test: /\.(woff(2)?|eot|ttf|otf)$/,
         type: 'asset/inline'},
       {
         test: /\.(css)$/,
